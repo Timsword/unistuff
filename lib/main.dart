7206ce611c883a4628c517b8275b796875ec2126
Tim's <trolllca@hotmail.com>
2021-12-31 16:05:28 +0300

    repistory update

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
-import 'package:chat_unistaff_project/screens/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:unistuff_main/screens/wrapper.dart';
+import 'package:provider/provider.dart';
+import 'package:unistuff_main/services/auth.dart';
+import 'package:unistuff_main/models/MyUser.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return StreamProvider<MyUser?>.value(
+      catchError: (User, MyUser) => null,
+      initialData: null,
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
       ),
-      home: HomePage()
     );
   }
 }
